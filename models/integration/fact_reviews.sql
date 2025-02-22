7cde7b9baee30d6e4069d945f1e81baa541f5782
vmeht02git <vmehta02@yahoo.com>
2025-02-22 18:50:10 +0530

    incremental strategy for fact and cte populated

--- models/integration/fact_reviews.sql
+++ models/integration/fact_reviews.sql
@@ -1,5 +1,9 @@
-{{config(materialized= 'incremental')}}
-
+{{config(
+    materialized= 'incremental',
+    merge_update_column=['review_text'],
+    unique_key=['listing_id','reviewer_name']
+)
+}}
 SELECT * FROM {{ref('src_reviews')}} WHERE 1=1 
 {% if is_incremental() %}
     AND review_date > (SELECT MAX(review_date) FROM {{this}})
